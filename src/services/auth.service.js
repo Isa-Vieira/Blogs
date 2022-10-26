a1db89b5b6385db58e4cca005f306986905af6bd
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-10-25 21:01:21 -0300

    erros

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -28,10 +28,12 @@ const validateLogin = async ({ email, password }) => {
 };
 
 const validateToken = (token) => {
-    if (!token) {
-      const tokenNum = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjo1LCJkaXNwbGF5TmFtZSI6InVzdWFyaW8gZGUgdGVzdGUiLCJlbWFpbCI6InRlc3RlQGVtYWlsLmNvbSIsImltYWdlIjoibnVsbCJ9LCJpYXQiOjE2MjAyNDQxODcsImV4cCI6MTYyMDY3NjE4N30.Roc4byj6mYakYqd9LTCozU1hd9k_Vw5IWKGL4hcCVG8';
-        return { type: 200, mssage: tokenNum };
-    }
+    /* if (!token) {
+        const tokenNum = {
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjo1LCJkaXNwbGF5TmFtZSI6InVzdWFyaW8gZGUgdGVzdGUiLCJlbWFpbCI6InRlc3RlQGVtYWlsLmNvbSIsImltYWdlIjoibnVsbCJ9LCJpYXQiOjE2MjAyNDQxODcsImV4cCI6MTYyMDY3NjE4N30.Roc4byj6mYakYqd9LTCozU1hd9k_Vw5IWKGL4hcCVG8',
+        return { type:200, mssage: tokenNum}
+          };
+    } */
 
     const user = jwtUtil.validateToken(token);
 
